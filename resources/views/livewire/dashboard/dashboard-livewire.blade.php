9a11d4a24f1d161e286b224f3df1799b80adfa13
EJyoungc <nerst@live.com>
2024-02-17 12:53:06 +0200

    main

--- resources/views/livewire/dashboard/dashboard-livewire.blade.php
+++ resources/views/livewire/dashboard/dashboard-livewire.blade.php
@@ -2,21 +2,21 @@
     {{-- The whole world belongs to you. --}}
 
     <div class="row">
-        <div class="col-12 col-md-6 col-lg-4 mb-2">
+        <div class="col-12 col-md-4 col-lg-4 mb-2">
             <div class="card bg-primary text-light ">
                 <div class="card-body">
                     <h4>Types</h4>
                 </div>
             </div>
         </div>
-        <div class="col-12 col-md-6 col-lg-4 mb-2">
+        <div class="col-12 col-md-4 col-lg-4 mb-2">
             <div class="card bg-success text-light ">
                 <div class="card-body">
                     <h4>Tasks</h4>
                 </div>
             </div>
         </div>
-        <div class="col-12 col-md-6 col-lg-4 mb-2">
+        <div class="col-12 col-md-4 col-lg-4 mb-2">
             <div class="card bg-info text-light ">
                 <div class="card-body">
                     <h4>Projects</h4>

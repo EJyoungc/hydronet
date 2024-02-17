22fc18ae6d719307976f8b439b0befc8df186683
EJyoungc <nerst@live.com>
2024-02-17 13:00:32 +0200

    main

--- app/Livewire/Dashboard/DashboardLivewire.php
+++ app/Livewire/Dashboard/DashboardLivewire.php
@@ -2,12 +2,21 @@
 
 namespace App\Livewire\Dashboard;
 
+use App\Models\Hydrophonic;
+use App\Models\Readings;
+use App\Models\Type;
 use Livewire\Component;
 
 class DashboardLivewire extends Component
 {
     public function render()
     {
-        return view('livewire.dashboard.dashboard-livewire');
+        $projects = Hydrophonic::all();
+        $tasks = Readings::all();
+        $types = Type::all();
+        return view('livewire.dashboard.dashboard-livewire')
+        ->with('projects',$projects)
+        ->with('tasks',$tasks)
+        ->with('types',$types);
     }
 }
--- resources/views/livewire/dashboard/dashboard-livewire.blade.php
+++ resources/views/livewire/dashboard/dashboard-livewire.blade.php
@@ -5,24 +5,36 @@
         <div class="col-12 col-md-4 col-lg-4 mb-2">
             <div class="card bg-primary text-light ">
                 <div class="card-body">
-                    <h4>Types</h4>
+                    <div class="d-flex justify-content-between">
+                        <h4>Types</h4>
+                        <h4>{{ $types->count() }}</h4>
+                    </div>
+
                 </div>
             </div>
         </div>
         <div class="col-12 col-md-4 col-lg-4 mb-2">
             <div class="card bg-success text-light ">
                 <div class="card-body">
-                    <h4>Tasks</h4>
+                    <div class="d-flex justify-content-between">
+                        <h4>Tasks</h4>
+                        <h4>{{ $tasks->count() }}</h4>
+                    </div>
+
                 </div>
             </div>
         </div>
         <div class="col-12 col-md-4 col-lg-4 mb-2">
             <div class="card bg-info text-light ">
                 <div class="card-body">
-                    <h4>Projects</h4>
+                    <div class="d-flex justify-content-between">
+                        <h4>Projects</h4>
+                        <h4>{{ $projects->count() }}</h4>
+                    </div>
+
                 </div>
             </div>
         </div>
-        
+
     </div>
 </div>

6b852a16821bf8d8b2af6a6e45e63e360fba7745
EJyoungc <nerst@live.com>
2024-02-21 13:03:43 +0200

    main

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/test', function (Request $request) {
+        return response('Hello World', 200);
+});
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
